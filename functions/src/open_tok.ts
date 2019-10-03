9500a2d6855dea878a2be4731e89facdc78e7ee5
frenchdonuts <jonathantan1991@gmail.com>
2019-10-03 16:11:04 -0700

    Require name and avatarURL to generate otToken;

--- functions/src/open_tok.ts
+++ functions/src/open_tok.ts
@@ -17,10 +17,19 @@ export const genToken = functions.https.onCall((data, context) => {
   if (!context.auth || !context.auth.uid)
     return Promise.reject(`Unauthenticated attempt to create OT session.`)
   const sessionId = data.sessionId
-  if (!sessionId) return Promise.reject(`You can't generate an OT token w/o a session id`)
+  const uid = context.auth.uid
+  const name:string = data.name
+  const avatarURL:string = data.avatarURL
+  if (!sessionId || !name || !avatarURL) { 
+    const msg = `sessionId(=${sessionId}), name(=${name}), or avatarURL(=${avatarURL}) undefined`
+    return Promise.reject(msg)
+  }
 
   const ot = new OpenTok(apiKey, apiSecret)
-  return ({ token: ot.generateToken(sessionId, {}) })
+  const token = ot.generateToken(sessionId, {
+    data: `uid=${uid},name=${name},avatarURL=${avatarURL}`
+  })
+  return ({ token: token })
 })
 
 function createSession(ot: OpenTok, options: OpenTok.SessionOptions): Promise<OpenTok.Session> {
